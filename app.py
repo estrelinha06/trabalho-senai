c153da8837f64ecc46a68b9154f104533b3faee0
clara <clara@gmail.com>
2026-06-26 17:21:31 -0300

    26-06

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ app = Flask(__name__)
 def index():
      return render_template("index.html")
 
-@app.route("/teste")
-def teste():
-     return 'teste'
-
 @app.route("/home")
 def home():
      return render_template("home.html")
 
+@app.route("/cadastrarnovoitem")
+def cadastrarnovoitem():
+     return render_template("cadastrarnovoitem.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
